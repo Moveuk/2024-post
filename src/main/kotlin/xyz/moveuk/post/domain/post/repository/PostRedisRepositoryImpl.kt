6b20d4d3a6a389b80f8d5f8548de061554bfa535
moveuk <v_donguk@naver.com>
2024-02-07 16:31:50 +0900

    Bug: 최근 본 5개 게시물 조회 기능 버그 수정
    1. 최근 본 게시물 중복 처리 불가하도록 수정
    2. 최근 본 게시물 redis 저장시 키 상수화

--- src/main/kotlin/xyz/moveuk/post/domain/post/repository/PostRedisRepositoryImpl.kt
+++ src/main/kotlin/xyz/moveuk/post/domain/post/repository/PostRedisRepositoryImpl.kt
@@ -8,9 +8,11 @@ class PostRedisRepositoryImpl(
     private val redisTemplate: RedisTemplate<String, String>
 ) : PostRedisRepository {
     override fun saveRecentPostIds(memberId: Long, todoId: Long) {
-        val key = "recentPost_$memberId"
+        val key = "$RECENT_POST_KEY$memberId"
 
         redisTemplate.opsForList().let {
+            it.remove(key, 0, todoId.toString())
+
             it.leftPush(key, todoId.toString())
 
             if (it.size(key)!! > RECENT_POST_COUNT) {
@@ -20,12 +22,13 @@ class PostRedisRepositoryImpl(
     }
 
     override fun getRecentPostIds(memberId: Long): List<Long>? {
-        val key = "recentPost_$memberId"
+        val key = "$RECENT_POST_KEY$memberId"
 
-        return redisTemplate.opsForList().range(key, 0, RECENT_POST_COUNT + 1)?.map { it.toLong() }
+        return redisTemplate.opsForList().range(key, 0, RECENT_POST_COUNT)?.map { it.toLong() }
     }
 
     companion object {
         const val RECENT_POST_COUNT = 5L
+        const val RECENT_POST_KEY = "recentPost_"
     }
 }
